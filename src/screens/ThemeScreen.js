8efc160b83686be3a540ea4e8c06860edf01cd12
hjpnam <hjpnam@gmail.com>
2021-03-07 22:24:43 -0600

    took out logo

--- src/screens/ThemeScreen.js
+++ src/screens/ThemeScreen.js
@@ -40,16 +40,16 @@ const ThemeScreen = ({navigation}) => {
   }
 
   const themeList =
-  [{title: "excited", color: "#ce51c6", description: "Excitement is a feeling and mood full of activity, joy, exhilaration and upheaval.\n\nAromatherapy: Blend of Grapefruit, Bergamot and Lemon\nLight Status: Red\nPlaying: Beach Waves"},
-  {title: "chill", color: "#72b583", description: "By finding balance in your everyday life, you can bring so much positivity and light to your current mood\n\nAromatherapy: Blend of Clove, Patchouli, Sandalwood\nLight Status: Green\nPlaying: Japanese Garden"},
-  {title: "happy", color: "#eac752", description: "The key to being happy is knowing you have the power to choose what to accept and what to let go\n\nAromatherapy: Rose\nLight Status: Blend of Reds\nPlaying: Love is in the air"},
-  {title: "focused", color: "#27D507", description: "The secret of change is to focus all your energy, not on fighting the old, but on building the new\n\nAromatherapy: Blend of Wintergreen, Rosemary and Peppermint\nLight Status: Lime Green\nPlaying: Soothing Sounds"},
-  {title: "calm", color: "#72bbe2", description: "A calm heart is always less stressful. Free your mind and heart from anxiety, anger and have peace within your soul\n\nAromatherapy: Blend of Lavender\nLight Status: Blue\nPlaying: Nature Waves"},
-  {title: "relaxed", color: "#c59cd6", description: "To relax means to liberate the mind, the body, or both.  Relaxing can quiet your mind, make you feel peaceful and live in the present moment. Take a deep breath, and enjoy life\n\nAromatherapy: Grapefruit\nPlaying: Peaceful Waterfall"},
-  {title: "motivated", color: "#64b6b3", description: "Motivation reflects something unique about each one of us and allows us to gain valued outcomes like improved performance, enhanced well-being, personal growth, and a sense of purpose\n\nAromatherapy: Bergamont, Tangeringe, LIme\nLight Status: Cyan\nPlaying: Morning Glory"},
-  {title: "energized", color: "#e2a862", description: "Energize means to feel enthusiasm and determination to do something. The higher your energy level, the more efficient your body and the better you feel\n\nAromatherapy: Frankincense\nLight Status: Orange\nPlaying: Feeling Strong"},
-  {title: "empowered", color: "#9242b7", description: "Empowerment reflects something unique about each one of us and allows us to gain valued outcomes like improved performance, enhanced well-being, personal growth, and a sense of purpose\n\nAromatherapy: Bergamont, Tangeringe, and Lime\nLight Status: Purple\nPlaying: Winners Circle"},
-  {title: "romantic", color: "#da3a7d", description: "Romance is a powerful force that makes you feel connected. You always gain by giving love\n\nAromatherapy: Rose\nLight Status: Pink\nPlaying: Love is in the Air"}];
+    [{title: "excited", color: "#ce51c6", description: "Excitement is a feeling and mood full of activity, joy, exhilaration and upheaval.\n\nAromatherapy: Blend of Grapefruit, Bergamot and Lemon\nLight Status: Red\nPlaying: Beach Waves"},
+    {title: "chill", color: "#72b583", description: "By finding balance in your everyday life, you can bring so much positivity and light to your current mood\n\nAromatherapy: Blend of Clove, Patchouli, Sandalwood\nLight Status: Green\nPlaying: Japanese Garden"},
+    {title: "happy", color: "#eac752", description: "The key to being happy is knowing you have the power to choose what to accept and what to let go\n\nAromatherapy: Rose\nLight Status: Blend of Reds\nPlaying: Love is in the air"},
+    {title: "focused", color: "#27D507", description: "The secret of change is to focus all your energy, not on fighting the old, but on building the new\n\nAromatherapy: Blend of Wintergreen, Rosemary and Peppermint\nLight Status: Lime Green\nPlaying: Soothing Sounds"},
+    {title: "calm", color: "#72bbe2", description: "A calm heart is always less stressful. Free your mind and heart from anxiety, anger and have peace within your soul\n\nAromatherapy: Blend of Lavender\nLight Status: Blue\nPlaying: Nature Waves"},
+    {title: "relaxed", color: "#c59cd6", description: "To relax means to liberate the mind, the body, or both.  Relaxing can quiet your mind, make you feel peaceful and live in the present moment. Take a deep breath, and enjoy life\n\nAromatherapy: Grapefruit\nPlaying: Peaceful Waterfall"},
+    {title: "motivated", color: "#64b6b3", description: "Motivation reflects something unique about each one of us and allows us to gain valued outcomes like improved performance, enhanced well-being, personal growth, and a sense of purpose\n\nAromatherapy: Bergamont, Tangeringe, LIme\nLight Status: Cyan\nPlaying: Morning Glory"},
+    {title: "energized", color: "#e2a862", description: "Energize means to feel enthusiasm and determination to do something. The higher your energy level, the more efficient your body and the better you feel\n\nAromatherapy: Frankincense\nLight Status: Orange\nPlaying: Feeling Strong"},
+    {title: "empowered", color: "#9242b7", description: "Empowerment reflects something unique about each one of us and allows us to gain valued outcomes like improved performance, enhanced well-being, personal growth, and a sense of purpose\n\nAromatherapy: Bergamont, Tangeringe, and Lime\nLight Status: Purple\nPlaying: Winners Circle"},
+    {title: "romantic", color: "#da3a7d", description: "Romance is a powerful force that makes you feel connected. You always gain by giving love\n\nAromatherapy: Rose\nLight Status: Pink\nPlaying: Love is in the Air"}];
   const [selectedTheme, setSelectedTheme] = useState(themeList[0]);
 
   if (fontsLoaded) {
@@ -90,21 +90,20 @@ const ThemeScreen = ({navigation}) => {
     </TouchableOpacity >
   );
   const renderItem = ({item}) => (
-    <Item title={item.title} color={item.color} description={item.description}/>
+    <Item title={item.title} color={item.color} description={item.description} />
   );
   return (
     <ScrollView>
       <ThemeModal isVisible={modalVisible} theme={selectedTheme} toggleModal={toggleModal} navigateTheme={navigateTheme} />
       <BeaconButton navigation={navigation} />
       <SafeAreaView style={styles.container}>
-        <Logo/>
         <View style={styles.userSection}>
           <View>
             <Text style={styles.textStyle}>Hi {user ? user.name : 'guest'},</Text>
           </View>
           <View>
-            {user ? <Button title="Logout" color="#448aff" onPress={handleSignout}/> 
-            : <Button title="signin" color="#448aff" onPress={handleSignin}/>}
+            {user ? <Button title="Logout" color="#448aff" onPress={handleSignout} />
+              : <Button title="signin" color="#448aff" onPress={handleSignin} />}
           </View>
         </View>
         <Text style={styles.textBold}>how would you like to feel?</Text>
